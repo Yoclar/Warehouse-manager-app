1c03e870db9e5926f4b72dcd019c897717d54e0f
Ricsi <thereallemonmailgmail.com>
2023-10-20 16:43:50 +0200

    SQL connection set

---
+++ Warehouse/Warehouse/DataBaseConnect.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace Warehouse
+{
+    public class DataBaseConnect
+    {
+        private MySqlConnection connection;
+
+        // Constructor
+        public DataBaseConnect(string server, string database, string uid, string password)
+        {
+            Initialize(server, database, uid, password);
+        }
+
+        // Initialize values
+        private void Initialize(string server, string database, string uid, string password)
+        {
+            string connectionString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={password};";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        // Open the connection
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        // Close the connection
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlDataReader ExecuteQuery(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            return cmd.ExecuteReader();
+        }
+
+        public int ExecuteInsert(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            return cmd.ExecuteNonQuery();
+
+        }
+    }
+}
---
+++ Warehouse/Warehouse/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace Warehouse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string server = "localhost";
+            string database = "warehouse";
+            string uid = "root";
+            string password = "";
+
+            // Create an instance of the DataBaseConnect class
+            var db = new DataBaseConnect(server, database, uid, password);
+
+            // Open the connection
+            db.OpenConnection();
+
+            try
+            {
+                // Perform your database operations here
+                // ...
+
+                string query = "SELECT * FROM `items`";
+
+                MySqlDataReader reader = db.ExecuteQuery(query);
+                
+                string result = string.Empty;
+
+                while (reader.Read()) 
+                {
+                    string columnValue = reader["Name"].ToString()!;
+
+
+                    result += columnValue + Environment.NewLine;
+                }
+                queryLabel.Content= result;
+
+                
+
+                UpdateStatusText("..Success");
+
+                // Close the connection when done
+                db.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during database operations
+                UpdateStatusText("An error occurred: " + ex.Message);
+
+                // Ensure the connection is still closed
+                db.CloseConnection();
+            }
+        }
+
+
+        private void UpdateStatusText(string message)
+        {
+            // Update a TextBlock (statusTextBlock) or Label with the message
+            statusTextBlock.Content = message;
+        }
+    }
+}
